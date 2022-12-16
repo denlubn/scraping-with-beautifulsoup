9af7febc1b500256267f53752c72a692e687777a
Denis Pidlubniy <denispodlubnyj@gmail.com>
2022-12-16 18:58:55 +0200

    Added Selenium to get price depending on HDD

--- parse.py
+++ parse.py
@@ -7,6 +7,10 @@ from urllib.parse import urljoin
 
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import ElementClickInterceptedException
 
 
 BASE_URL = "https://webscraper.io/"
@@ -22,12 +26,26 @@ class Product:
     price: float
     rating: int
     num_of_reviews: int
+    additional_info: dict
+    url: str
 
 
 PRODUCT_FIELDS = [field.name for field in fields(Product)]
 
+_driver: WebDriver | None = None
+
+
+def get_driver() -> WebDriver:
+    return _driver
+
+
+def set_driver(new_driver: WebDriver) -> None:
+    global _driver
+    _driver = new_driver
+
+
 logging.basicConfig(
-    level=logging.DEBUG,
+    level=logging.INFO,
     format="[%(levelname)8s]: %(message)s",
     handlers=[
         logging.FileHandler(os.path.join("parser.log")),
@@ -36,7 +54,36 @@ logging.basicConfig(
 )
 
 
+def parse_hdd_block_prices(product_soup: BeautifulSoup) -> dict[str, float]:
+    detailed_url = urljoin(BASE_URL, product_soup.select_one(".title")["href"])
+    driver = get_driver()
+    driver.get(detailed_url)
+    swatches = driver.find_element(By.CLASS_NAME, "swatches")
+    buttons = swatches.find_elements(By.TAG_NAME, "button")
+
+    prices = {}
+
+    for button in buttons:
+        if not button.get_property("disabled"):
+            is_clickable = False
+            while not is_clickable:
+                try:
+                    button.click()
+                except ElementClickInterceptedException:
+                    continue
+                else:
+                    is_clickable = True
+
+            prices[button.get_property("value")] = float(driver.find_element(
+                By.CLASS_NAME, "price"
+            ).text.replace("$", ""))
+
+    return prices
+
+
 def parse_single_product(product_soup: BeautifulSoup) -> Product:
+    hdd_prices = parse_hdd_block_prices(product_soup)
+
     return Product(
         title=product_soup.select_one(".title")["title"],
         description=product_soup.select_one(".description").text,
@@ -45,6 +92,8 @@ def parse_single_product(product_soup: BeautifulSoup) -> Product:
         num_of_reviews=int(
             product_soup.select_one(".ratings > p.pull-right").text.split()[0]
         ),
+        additional_info={"hdd_prices": hdd_prices},
+        url=urljoin(BASE_URL, product_soup.select_one(".title")["href"]),
     )
 
 
@@ -68,11 +117,10 @@ def get_laptop_products() -> [Product]:
     page = requests.get(LAPTOP_URL).content
     first_page_soup = BeautifulSoup(page, "html.parser")
 
-    # get nums of pages
     num_pages = get_num_pages(first_page_soup)
 
     all_products = get_single_page_products(first_page_soup)
-    #iterate on pages & get all products from single page
+
     for page_num in range(2, num_pages + 1):
         logging.info(f"Start parsing page #{page_num}")
         page = requests.get(LAPTOP_URL, {"page": page_num}).content
@@ -83,15 +131,17 @@ def get_laptop_products() -> [Product]:
 
 
 def write_products_to_csv(products: [Product]) -> None:
-    with open(PRODUCTS_OUTPUT_CSV_PATH, "w") as file:
+    with open(PRODUCTS_OUTPUT_CSV_PATH, "w", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerow(PRODUCT_FIELDS)
         writer.writerows([astuple(product) for product in products])
 
 
 def main():
-    products = get_laptop_products()
-    write_products_to_csv(products)
+    with webdriver.Chrome() as new_driver:
+        set_driver(new_driver)
+        products = get_laptop_products()
+        write_products_to_csv(products)
 
 
 if __name__ == "__main__":
